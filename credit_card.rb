4a54ea7bd377fb40e060c21fcec70b8e308285f4
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2021-03-18 12:27:00 +0800

    hw3 done

--- credit_card.rb
+++ credit_card.rb
@@ -3,19 +3,22 @@ require 'json'
 
 class CreditCard
   # TODO: mixin the LuhnValidator using an 'include' statement
-
+  include LuhnValidator
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
     # TODO: initialize the instance variables listed above
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
   end
 
   # returns json string
-  def to_json
-    {
-      # TODO: setup the hash with all instance vairables to serialize into json
-    }.to_json
+  def to_json(_)
+    # TODO: setup the hash with all instance vairables to serialize into json
+    hash.to_json
   end
 
   # returns all card information as single string
@@ -26,6 +29,7 @@ class CreditCard
   # return a new CreditCard object given a serialized (JSON) representation
   def self.from_s(card_s)
     # TODO: deserializing a CreditCard object
+    JSON.parse(card_s)
   end
 
   # return a hash of the serialized credit card object
@@ -34,6 +38,12 @@ class CreditCard
     #   - Produce a hash (using default hash method) of the credit card's
     #     serialized contents.
     #   - Credit cards with identical information should produce the same hash
+    { 
+      owner: @owner,
+      number: @number,
+      expiration_date: @expiration_date,
+      credit_network: @credit_network
+    }
   end
 
   # return a cryptographically secure hash
@@ -41,5 +51,6 @@ class CreditCard
     # TODO: implement this method
     #   - Use sha256 from openssl to create a cryptographically secure hash.
     #   - Credit cards with identical information should produce the same hash
+    OpenSSL::Digest.new('SHA256').digest(to_s)
   end
 end
--- luhn_validator.rb
+++ luhn_validator.rb
@@ -5,7 +5,16 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
-
+    sum = 0
     # TODO: use the integers in nums_a to validate its last check digit
-  end
+    nums_a.reverse.map.with_index do |digit, index|
+      if(index.odd?)
+        temp = digit * 2
+        sum += (temp>=10) ? temp - 9 : temp
+      end
+      sum += digit
+    end
+
+    return sum.modulo(10).zero?
+  end 
 end
